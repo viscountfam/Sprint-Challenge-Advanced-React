ac78a9e28fc8866bce9b46e0bc6b05ab16ccfae4
viscountfam <V3ctor42!>
2020-01-17 13:14:35 -0500

    basic tests added

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { render } from '@testing-library/react'
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+
+test('All Player cards are rendered', () => {
+  const { getAllByText } = render(<App/>);
+  const countries = getAllByText(/country/i);
+  expect(countries).toHaveLength(101);
 });
+
+test('Marta exists', () => {
+  const {getByText} = render (<App/>);
+  const marta = getByText(/marta/i);
+  expect(marta).toBeInTheDocument();
+  const 
+})
