8ca61637a37d47e2b792b0744e20f0603f0d32e1
viscountfam <V3ctor42!>
2020-01-17 12:58:24 -0500

    3/4 MVP completed

--- client/src/components/PlayerCards.js
+++ client/src/components/PlayerCards.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { useDarkMode } from '../hooks/useDarkMode'
 
+
 export default function PlayerCards(props) {
-   
+  const [darkMode, setDarkMode] = useDarkMode(false);
+  const lightsOut = e => {
+      e.preventDefault();
+      setDarkMode(true)
+      console.log(darkMode)
+  }
     return (
         <div className="player-cards">
             <h3>{props.player.name}</h3>
             <p>Country: {props.player.country}</p>
             <p>Searches: {props.player.searches}</p>
+            <button onClick={lightsOut}>lights Out</button>
         </div>
     )
 }
--- client/src/hooks/useDarkMode.js
+++ client/src/hooks/useDarkMode.js
@@ -1,14 +1,12 @@
-import {useLocalStorage} from "./useLocalStorage"
-import {useEffect} from 'react'
-export const useDarkMode = (initialValue) => {
-    const [darkMode, setDarkMode] = useLocalStorage(darkMode, initialValue);
+
+import {useState, useEffect} from 'react'
+export const useDarkMode = () => {
+    const [darkMode, setDarkMode] = useState()
     useEffect(() => {
         if(darkMode === true) { 
-            document.body.classList.add("dark-mode");
-            document.getElementsByClassName("player-cards").classList.add("dark-mode-card")
-        } else if(darkMode === false) {
-            document.body.classList.remove("dark-mode")
-            document.getElementsByClassName("player-cards").classList.remove("dark-mode-card")
+            document.body.classList.add("dark-mode")
+        } if(darkMode === false) {
+            document.body.classList.remove("dark-mode-card")
         }
         }, [darkMode]);
 
